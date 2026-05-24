7d491d6558d7348bcff7fbdbfa4f0a9e1c49631e
gronQA <gronqa@gmail.com>
2026-05-24 19:48:52 +0200

    chore: configure for custom domain kropla-baltyku.pl

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import PrivacyPolicy from './pages/PrivacyPolicy';
 
 function App() {
   return (
-    <Router basename="/kropla-baltyku">
+    <Router>
       <ScrollToTop />
       <div className="flex flex-col min-h-screen">
         <Navbar />
